7a7c703f51894fbd981096ec572979cf1e9a3c00
Jose-Mario-Vargas <josevaragsg75@gmail.com>
2024-03-16 21:03:15 -0600

    new slider component

--- src/components/reactComponents/Card.jsx
+++ src/components/reactComponents/Card.jsx
@@ -1,21 +1,21 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
 import './Swiper.css'
+import { Navigation } from 'swiper/modules';
 
-function SwiperCard({title}) {
+export default function Card({title}) {
   return (
     <>
-    <div className="card">
+      <div className='card'>
       <h3 className="cardTitle">{title}</h3>
-       <Swiper className="mySwiper">
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
       <SwiperSlide className="slide"><img src="https://img.freepik.com/free-photo/person-using-laptop-using-artificial-intelligence-generate-images_23-2150905286.jpg?size=626&ext=jpg"/></SwiperSlide>
       <SwiperSlide className="slide"><img src="https://img.freepik.com/free-photo/person-using-ai-tool-job_23-2150711488.jpg?size=626&ext=jpg"/></SwiperSlide>
       <SwiperSlide className="slide"><img src="https://img.freepik.com/free-photo/person-using-laptop-using-artificial-intelligence-generate-images_23-2150905296.jpg?size=626&ext=jpg"/></SwiperSlide>
       <SwiperSlide className="slide"><img src="https://img.freepik.com/free-photo/person-laptop-using-artificial-intelligence-generate-image_23-2150571014.jpg?size=626&ext=jpg"/></SwiperSlide>
       </Swiper>
-    </div>
+      </div>
     </>
   );
 }
-
-export default SwiperCard;
