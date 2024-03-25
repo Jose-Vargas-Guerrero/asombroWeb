3b1664a5b32f9377ab0e19c3fa1a0cf76b9a69c5
Jose-Mario-Vargas <josevaragsg75@gmail.com>
2024-03-24 23:00:22 -0600

    responsive design

--- src/components/reactComponents/Card.jsx
+++ src/components/reactComponents/Card.jsx
@@ -4,16 +4,17 @@ import 'swiper/css/navigation';
 import './Swiper.css'
 import { Navigation } from 'swiper/modules';
 
+
 export default function Card({title}) {
   return (
     <>
       <div className='card'>
       <h3 className="cardTitle">{title}</h3>
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-      <SwiperSlide className="slide"><img src="https://img.freepik.com/free-photo/person-using-laptop-using-artificial-intelligence-generate-images_23-2150905286.jpg?size=626&ext=jpg"/></SwiperSlide>
-      <SwiperSlide className="slide"><img src="https://img.freepik.com/free-photo/person-using-ai-tool-job_23-2150711488.jpg?size=626&ext=jpg"/></SwiperSlide>
-      <SwiperSlide className="slide"><img src="https://img.freepik.com/free-photo/person-using-laptop-using-artificial-intelligence-generate-images_23-2150905296.jpg?size=626&ext=jpg"/></SwiperSlide>
-      <SwiperSlide className="slide"><img src="https://img.freepik.com/free-photo/person-laptop-using-artificial-intelligence-generate-image_23-2150571014.jpg?size=626&ext=jpg"/></SwiperSlide>
+      <SwiperSlide className="slide"><img alt='imagen de servicios de la agencia Asombro' src="https://img.freepik.com/free-photo/person-using-laptop-using-artificial-intelligence-generate-images_23-2150905286.jpg?size=626&ext=jpg"/></SwiperSlide>
+      <SwiperSlide className="slide"><img alt='imagen de servicios de la agencia Asombro' src="https://img.freepik.com/free-photo/person-using-ai-tool-job_23-2150711488.jpg?size=626&ext=jpg"/></SwiperSlide>
+      <SwiperSlide className="slide"><img alt='imagen de servicios de la agencia Asombro' src="https://img.freepik.com/free-photo/person-using-laptop-using-artificial-intelligence-generate-images_23-2150905296.jpg?size=626&ext=jpg"/></SwiperSlide>
+      <SwiperSlide className="slide"><img alt='imagen de servicios de la agencia Asombro' src="https://img.freepik.com/free-photo/person-laptop-using-artificial-intelligence-generate-image_23-2150571014.jpg?size=626&ext=jpg"/></SwiperSlide>
       </Swiper>
       </div>
     </>
--- src/components/reactComponents/Menu.jsx
+++ src/components/reactComponents/Menu.jsx
@@ -1,41 +1,65 @@
 import { useState } from "react";
-import './Menu.css'
+import "./Menu.css";
 import { FaBars } from "react-icons/fa";
 import { IoCloseSharp } from "react-icons/io5";
-
-
+import { FaFacebook } from "react-icons/fa";
+import { BsInstagram } from "react-icons/bs";
 
 function Menu() {
-    const [hide, setHide] = useState(true)
-    
-    const handleHide =() => {
-        setHide(!hide)
-    }
+  const [hide, setHide] = useState(true);
 
+  const handleHide = () => {
+    setHide(!hide);
+  };
 
   return (
     <>
-    <FaBars className="icon" onClick={handleHide}/>
-    <SideMenu show={hide} handleHide={handleHide}/>
+      {hide ? (
+        <FaBars className="icon" onClick={handleHide} />
+      ) : (
+        <SideMenu show={hide} handleHide={handleHide} />
+      )}
     </>
   );
 }
 
-function SideMenu({handleHide,show}){
+function SideMenu({ handleHide, show }) {
   return (
-      <div class={show.toString()}>
-        <div>
-          <h2 className=" ">Asombro</h2>
-          <IoCloseSharp/>
-        </div>
-      <ul>
-        <li className="">menu</li>
+    <div className={show.toString()}>
+      <div className="header">
+        <h2 className=" ">Asombro</h2>
+        <IoCloseSharp onClick={handleHide} />
+      </div>
+      <ul className="links-container">
+        <a className="link" href="#hero" onClick={handleHide}>
+          Inicio
+        </a>
+        <a className="link" href="#QuienesSomos" onClick={handleHide}>
+          Quienes somos
+        </a>
+        <a className="link" href="#AsombroCreativo" onClick={handleHide}>
+          Asombro Creativo
+        </a>
+        <a className="link" href="#marcas" onClick={handleHide}>
+          Marcas
+        </a>
+        <a className="link" href="#servicios" onClick={handleHide}>
+          Servicios
+        </a>
+        <a className="link" href="#contactenos" onClick={handleHide}>
+          Contacto
+        </a>
       </ul>
+      <div className="icons-container">
+        <a href="https://www.instagram.com/asombrohn/" target="_blank">
+          <BsInstagram className="icon" />
+        </a>
+        <a href="https://www.facebook.com/asombrohn/?locale=es_LA" target="_blank">
+          <FaFacebook className="icon" />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
-
-
-
 
 export default Menu;
